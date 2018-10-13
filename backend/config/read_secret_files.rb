123f26fc9acc10bd39c0e3deacdf5029ad66f56f
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2018-10-13 14:38:40 -0500

    Read secret files for backend

---
+++ backend/config/read_secret_files.rb
@@ -0,0 +1,28 @@
+# Reads the specified secret paths (i.e. Docker Secrets) into environment
+# variables:
+
+# Process only a known list of env vars that can filled by reading a file (i.e.
+# a docker secret):
+Dir["#{ENV.fetch('SECRETS_PATH', '/run/secrets/')}*"].each do |secret_filepath|
+  secret_envvarname = File.basename(secret_filepath, ".*").upcase
+
+  # Skip if variable is already set - already-set variables have precedence over
+  # the secret files:
+  next if ENV.key?(secret_envvarname) && ENV[secret_envvarname].present?
+
+  ENV[secret_envvarname] = File.read(secret_filepath).strip
+end
+
+# Substitute the URL passwords with the ones stored on the *_PASS
+require 'uri' if (url_keys = ENV.keys.select { |key| key =~ /_URL/ }).any?
+
+url_keys.each do |url_key|
+  pass_key = url_key.gsub('_URL', '_PASSWORD')
+  next unless ENV.key?(url_key) && ENV.key?(pass_key)
+
+  uri = URI(ENV[url_key])
+  uri.password = ENV[pass_key]
+  ENV[url_key] = uri.to_s
+end
+
+# STDERR.puts ENV.inspect
