328002ee0dc206b8292c39044d4fa623942983c2
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2018-10-14 17:44:00 -0500

    Frontend: Update the dist/package.json config on reconfiguration

--- frontend/reconfigure-app.js
+++ frontend/reconfigure-app.js
@@ -17,13 +17,7 @@ function getEnvConfigNode(appName, appDOM) {
   return appDOM(`meta[name="${appName}/config/environment"]`);
 }
 
-function parseEnvConfigNode(appConfigNode) {
-  let metaContent = appConfigNode.attr('content');
-  let envConfigString = decodeURIComponent(metaContent);
-  return JSON.parse(envConfigString);
-}
-
-function updateEnvConfig(envConfig, envConfigNode) {
+function updateEnvConfigNode(envConfigNode, envConfig) {
   const envConfigString = JSON.stringify(envConfig);
   const metaContent = encodeURIComponent(envConfigString);
   envConfigNode.attr('content', metaContent);
@@ -33,20 +27,62 @@ function writeUpdatedDOM(updatedDOM, indexFilePath) {
   FS.writeFileSync(indexFilePath, updatedDOM.html(), 'utf8');
 }
 
+// Loads the ${DIST_PATH}/package.json into an object:
+function loadDistPackage(distPath) {
+  const packageFilePath = `${distPath}/package.json`;
+  const dataString = FS.readFileSync(packageFilePath, 'utf8');
+  return JSON.parse(dataString);
+}
+
+function processConfigWithEnv(config, ENV_PROCESSING) {
+  const { ENV_MAPPING, ENV_TRANSFORM } = ENV_PROCESSING;
+  Object.keys(config).forEach(function(configKey) {
+    Object.keys(config[configKey]).forEach(function(configSubKey) {
+      const envVarName = keyToEnvVarName(configSubKey);
+      const envMapping = ENV_MAPPING[envVarName] || 'APP';
+      if (envMapping == configKey) {
+        const [envTransformMethod, ...envTransformArgs] = ENV_TRANSFORM[envVarName] || ['toString'];
+        const envVarValue = process.env[envVarName];
+        if (typeof envVarValue != 'undefined' && envVarValue != '') {
+          config[envMapping][configSubKey] = envVarValue[envTransformMethod](...envTransformArgs);
+        }
+      }
+    })
+  });
+}
+
+function writeConfigToPackage(config, distPath) {
+  const configString = JSON.stringify(config);
+  const packageFilePath = `${distPath}/package.json`;
+  FS.writeFileSync(packageFilePath, configString, 'utf8');
+}
+
+function writeAppEnvToIndex(config, distPath) {
+  const { appName } = config.fastboot;
+  const appEnvConfig = config.fastboot.config[appName];
+
+  const indexFilePath = `${distPath}/index.html`;
+  const appDOM = loadDOM(indexFilePath);
+  const envConfigNode = getEnvConfigNode(appName, appDOM);
+  
+  updateEnvConfigNode(envConfigNode, appEnvConfig);
+  writeUpdatedDOM(appDOM, indexFilePath);
+}
+
 module.exports = {
-  fromProcessEnv: function(appName, distPath) {
-    const indexFilePath = `${distPath}/index.html`;
-    let appDOM = loadDOM(indexFilePath);
-    let envConfigNode = getEnvConfigNode(appName, appDOM);
-    let envConfig = parseEnvConfigNode(envConfigNode);
+  fromProcessEnv(distPath, ENV_PROCESSING) {
+    const config = loadDistPackage(distPath);
+    const { appName } = config.fastboot;
+    const appEnvConfig = config.fastboot.config[appName];
 
-    Object.keys(envConfig.APP).forEach(function(key) {
-      const envVarName = keyToEnvVarName(key);
-      const envVar = process.env[envVarName];
-      if (typeof envVar != 'undefined' && envVar != '') envConfig.APP[key] = envVar;
-    });
+    // Replace the root configuration, specially the 'fastboot.hostWhitelist' key:
+    processConfigWithEnv(config, ENV_PROCESSING);
 
-    updateEnvConfig(envConfig, envConfigNode);
-    writeUpdatedDOM(appDOM, indexFilePath);
+    // Replace the app configuration:
+    processConfigWithEnv(appEnvConfig, ENV_PROCESSING);
+
+    // From this point, 'config' has been processed, and we're ready to write down the files:
+    writeConfigToPackage(config, distPath);
+    writeAppEnvToIndex(config, distPath);
   }
 }
